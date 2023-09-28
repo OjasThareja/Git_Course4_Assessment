86681156e40cdbf14e5df79c57591b5188a63122
hp <ojasthareja99@gmail.com>
2023-09-28 13:22:08 +0530

    Sample Commit

---
+++ SamplePushToGit2/src/p1/c1.java
@@ -0,0 +1,11 @@
+package p1;
+import java.applet.*;
+
+public class c1 
+{
+	public void m1()
+	{
+		System.out.println("1");
+	}
+
+}
---
+++ SamplePushToGit2/src/p2/c2.java
@@ -0,0 +1,13 @@
+package p2;
+
+import p1.c1;
+
+public class c2 
+{
+	public static void main(String args[])
+	{
+		c1 obj1 = new c1();
+		obj1.m1();
+	}
+
+}
